e71341deaefa2c61b71c4620a847370c47e40e70
zmigliorini <zmigliorini@gmail.com>
2024-08-17 18:43:27 -0400

    added PrivateRoute component for redirecting users to login screen

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-const PrivateRoute = () => {
-  return <div>PrivateRoute</div>;
+//https://stackoverflow.com/questions/66412660/what-is-the-typescript-type-to-accept-any-kind-of-react-component-as-prop
+
+interface Props {
+  Child: React.ReactElement;
+}
+
+const PrivateRoute: React.FC<Props> = ({ Child }) => {
+  const auth = useAuth();
+
+  return auth.isLoggedIn ? Child : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
